076cf48b0236f65ce91af1e9991280b80627e29f
João Vitor Diniz Mistrinel <97896243+JohnDiniz@users.noreply.github.com>
2023-10-25 16:02:16 -0300

    fix errors auth

--- src/hooks/api/useGithubGetCode.js
+++ src/hooks/api/useGithubGetCode.js
@@ -1,9 +1,24 @@
 export default function useGithubGetCode() {
   const client_id = import.meta.env.VITE_CLIENT_ID;
+  const redirect_uri = import.meta.env.VITE_REDIRECT_URL;
 
   const githubGetCode = () => {
-    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${client_id}`)
-  }
+    const params = {
+      response_type: "code",
+      scope: "user public_repo",
+      client_id: client_id,
+      redirect_uri: redirect_uri,
+      state: "test-t5",
+    };
+
+    const queryString = Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join("&");
+
+    const authUrl = `https://github.com/login/oauth/authorize?${queryString}`;
+
+    window.location.assign(authUrl);
+  };
 
   return {
     githubGetCode
